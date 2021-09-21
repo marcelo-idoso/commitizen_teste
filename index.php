1597f6d30f4c6f0e2b6c14abf9122f20b8f9c208
Marcelo Pereira <marcelo_idoso@hotmail.com>
2021-09-21 12:43:02 -0300

    feat(printar versão do sistema): funcionalidade de Versão do Sistema
    Acrescetando no sistema funcionalidade para que seja possivel mostrar para o usuario qual versão do
    sistema esta rodando no nomento

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+class ApplicationVersion
+{
+    const MAJOR = 1;
+    const MINOR = 2;
+    const PATCH = 3;
+
+    public static function get()
+    {
+        $commitHash = trim(exec('git log --pretty="%h" -n1 HEAD'));
+
+        $commitDate = new \DateTime(trim(exec('git log -n1 --pretty=%ci HEAD')));
+        $commitDate->setTimezone(new \DateTimeZone('UTC'));
+
+        return sprintf('v%s.%s.%s-dev.%s (%s)', self::MAJOR, self::MINOR, self::PATCH, $commitHash, $commitDate->format('Y-m-d H:i:s'));
+    }
+}
+
+
+$teste = new ApplicationVersion();
+
+
+echo $teste->get();
